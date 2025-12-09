7907aeba9cd9e9f474e9e69a7f9a7edf9f4823db
Emilse Arias <emilsearias@gmail.com>
2025-12-09 11:18:45 -0300

    ejemplo de llamado de funciones usando commonjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const calculadora = require('./2-calculadoraParte1')
+
 console.log('🎅 Hola mundo desde Node.js');
 /*
 feature/funcionalidad
 fix/arreglo-bug
 realease/funciones
 */ 
+
+console.log('Sumamos dos numeros: ', calculadora.sumar(20,35))
+console.log('Restamos dos numeros: ', calculadora.restar(20,234))
