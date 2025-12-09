5a3e3fa6378d23791fb249a15d0cd7c7fafa3859
Emilse Arias <emilsearias@gmail.com>
2025-12-09 11:31:50 -0300

    agregamos un ejemplo para trabajar con commonjs y con ES module

---
+++ src/3-calculadoraParte2.js
@@ -0,0 +1,8 @@
+export const multiplicar = (num1, num2) =>{
+    return num1 * num2
+}
+
+export const dividir = (num1, num2) =>{
+    return num1 / num2
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const calculadora = require('./2-calculadoraParte1')
+import { multiplicar, dividir } from './3-calculadoraParte2.js';
+// const calculadora = require('./2-calculadoraParte1')
 
 console.log('🎅 Hola mundo desde Node.js');
 /*
@@ -7,5 +8,7 @@ fix/arreglo-bug
 realease/funciones
 */ 
 
-console.log('Sumamos dos numeros: ', calculadora.sumar(20,35))
-console.log('Restamos dos numeros: ', calculadora.restar(20,234))
+// console.log('Sumamos dos numeros: ', calculadora.sumar(20,35))
+// console.log('Restamos dos numeros: ', calculadora.restar(20,234))
+console.log('Multiplicamos dos numeros: ', multiplicar(5,35))
+console.log('Dividimos dos numeros: ', dividir(250,20))
