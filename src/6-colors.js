25e491891bbcabd508c8787041856feda5960bce
Emilse Arias <emilsearias@gmail.com>
2025-12-11 10:37:44 -0300

    agregamos un ejemplo de modulos de tercero

---
+++ src/6-colors.js
@@ -0,0 +1,6 @@
+const colors = require('colors')
+
+console.error('Ocurrio un error'.bgRed.white)
+console.log('Agregamos canciones'.green)
+console.info('Ejemplo de texto subrayado'.underline.cyan)
+console.log('Ejemplo de texto 2'.rainbow)
