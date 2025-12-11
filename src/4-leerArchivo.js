37381441da804358ca3a799fd5aa31c21763d4b4
Emilse Arias <emilsearias@gmail.com>
2025-12-11 10:43:15 -0300

    agregamos el modulo de colores en el ejemplo de leerArchivo

--- src/4-leerArchivo.js
+++ src/4-leerArchivo.js
@@ -1,10 +1,12 @@
 //ejemplo de módulo fs
 const fs = require('fs')
+const colors = require('colors')
+
 //import fs from 'fs'
 
 try{
     const data = fs.readFileSync('./canciones.txt','utf-8')
     console.log(data)
 }catch(error){
-    console.error('ocurrio un error al leer el archivo: ',error)
+    console.error(`${'ocurrio un error al leer el archivo:'.bgRed.white} \n ${error}`)
 }
